c5ac77755c4087f9975bbcd6f9aba84006e2ea28
James Strachan <james.strachan@gmail.com>
2015-03-02 12:29:47 +0000

    make builds real time and polish UI

--- plugins/kubernetes/ts/builds.ts
+++ plugins/kubernetes/ts/builds.ts
@@ -4,75 +4,80 @@
 module Kubernetes {
 
   export var BuildsController = controller("BuildsController", ["$scope", "KubernetesModel", "KubernetesBuilds", "KubernetesState", "$dialog", "$window", "$templateCache", "$routeParams", "$location", "localStorage", "$http", "$timeout", "KubernetesApiURL",
-    ($scope, KubernetesModel: Kubernetes.KubernetesModelService, KubernetesBuilds, KubernetesState, $dialog, $window, $templateCache, $routeParams, $location:ng.ILocationService, localStorage, $http, $timeout, KubernetesApiURL) => {
+    ($scope, KubernetesModel:Kubernetes.KubernetesModelService, KubernetesBuilds, KubernetesState, $dialog, $window, $templateCache, $routeParams, $location:ng.ILocationService, localStorage, $http, $timeout, KubernetesApiURL) => {
 
-    $scope.kubernetes = KubernetesState;
-    $scope.model = KubernetesModel;
-    $scope.KubernetesBuilds = KubernetesBuilds;
-    $scope.$on('kubernetesModelUpdated', function () {
-      Core.$apply($scope);
-    });
+      $scope.kubernetes = KubernetesState;
+      $scope.model = KubernetesModel;
+      $scope.KubernetesBuilds = KubernetesBuilds;
+      $scope.$on('kubernetesModelUpdated', function () {
+        Core.$apply($scope);
+      });
 
-    $scope.tableConfig = {
-      data: 'builds',
-      showSelectionCheckbox: true,
-      enableRowClickSelection: false,
-      multiSelect: true,
-      selectedItems: [],
-      filterOptions: {
-        filterText: $location.search()["q"] || ''
-      },
-      columnDefs: [
-        {
-          field: 'metadata.name',
-          displayName: 'Name',
-          cellTemplate: $templateCache.get("buildLinkTemplate.html")
+      $scope.tableConfig = {
+        data: 'builds',
+        showSelectionCheckbox: true,
+        enableRowClickSelection: false,
+        multiSelect: true,
+        selectedItems: [],
+        filterOptions: {
+          filterText: $location.search()["q"] || ''
         },
-        {
-          field: 'status',
-          displayName: 'Status',
-          cellTemplate: $templateCache.get("buildStatusTemplate.html")
-        },
-        {
-          field: 'parameters.source.type',
-          displayName: 'Source'
-        },
-        {
-          field: 'parameters.source.git.uri',
-          displayName: 'Repository',
-          cellTemplate: $templateCache.get("buildRepositoryTemplate.html")
-        },
-        {
-          field: 'parameters.strategy.type',
-          displayName: 'Strategy'
-        },
-        {
-          field: 'parameters.strategy.stiStrategy.image',
-          displayName: 'Source Image'
-        },
-        {
-          field: 'parameters.output.imageTag',
-          displayName: 'Output Image'
-        }]
-    };
+        columnDefs: [
+          {
+            field: 'metadata.name',
+            displayName: 'Name',
+            cellTemplate: $templateCache.get("buildLinkTemplate.html")
+          },
+          {
+            field: 'status',
+            displayName: 'Status',
+            cellTemplate: $templateCache.get("buildStatusTemplate.html")
+          },
+          {
+            field: 'parameters.source.type',
+            displayName: 'Source'
+          },
+          {
+            field: 'parameters.source.git.uri',
+            displayName: 'Repository',
+            cellTemplate: $templateCache.get("buildRepositoryTemplate.html")
+          },
+          {
+            field: 'parameters.strategy.type',
+            displayName: 'Strategy'
+          },
+          {
+            field: 'parameters.strategy.stiStrategy.image',
+            displayName: 'Source Image'
+          },
+          {
+            field: 'parameters.output.imageTag',
+            displayName: 'Output Image'
+          }]
+      };
 
-    Kubernetes.initShared($scope, $location, $http, $timeout, $routeParams, KubernetesModel, KubernetesState, KubernetesApiURL);
+      Kubernetes.initShared($scope, $location, $http, $timeout, $routeParams, KubernetesModel, KubernetesState, KubernetesApiURL);
 
-      function updateData() {
+      $scope.$keepPolling = () => keepPollingModel;
+      $scope.fetch = PollHelpers.setupPolling($scope, (next:() => void) => {
         var url = buildsRestURL;
         $http.get(url).
           success(function (data, status, headers, config) {
             if (data) {
-              console.log("got data " + angular.toJson(data, true));
+              //console.log("got data " + angular.toJson(data, true));
               $scope.builds = data.items;
               $scope.fetched = true;
             }
+            Core.$apply($scope);
+            next();
           }).
           error(function (data, status, headers, config) {
             log.warn("Failed to load " + url + " " + data + " " + status);
+            Core.$apply($scope);
+            next();
           });
-      }
+      });
 
-      updateData();
-  }]);
+      $scope.fetch();
+    }]);
 }
