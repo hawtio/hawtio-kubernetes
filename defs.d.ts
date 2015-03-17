e59d94e698bd299aa4398ba138e95eb441f88c4c
James Strachan <james.strachan@gmail.com>
2015-03-17 15:30:24 +0000

    added a link to a build page to the pod

--- defs.d.ts
+++ defs.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="d.ts/includes.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/kubernetesHelpers.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/kubernetesModel.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/kubernetesPlugin.d.ts"/>
