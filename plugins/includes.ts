48b68311323af3c79db59d76c270147d715036f6
James Strachan <james.strachan@gmail.com>
2015-06-09 09:37:47 +0100

    used latest forge

--- plugins/includes.ts
+++ plugins/includes.ts
@@ -1,3 +1,4 @@
+/// <reference path="../libs/hawtio-forge/defs.d.ts"/>
 /// <reference path="../libs/hawtio-forms/defs.d.ts"/>
 /// <reference path="../libs/hawtio-ui/defs.d.ts"/>
 /// <reference path="../libs/hawtio-utilities/defs.d.ts"/>
