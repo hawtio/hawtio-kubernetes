38f7f6acfb6074a325db896eee645685a044d762
James Strachan <james.strachan@gmail.com>
2015-09-24 16:56:23 +0100

    lets hide the old 2 level nav bar

--- plugins/kubernetes/ts/kubernetesPlugin.ts
+++ plugins/kubernetes/ts/kubernetesPlugin.ts
@@ -47,7 +47,7 @@ module Kubernetes {
                   .when(UrlHelpers.join(context, 'imageRepositories'), route('imageRepositories.html', false))
                   .when(UrlHelpers.join(context, 'pipelines'), route('pipelines.html', false))
                   .when(UrlHelpers.join(context, 'overview'), route('overview.html', true))
-                  .when(context, { redirectTo: UrlHelpers.join(context, 'apps') });
+                  .when(context, { redirectTo: "/workspaces" });
   }]);
   
 
