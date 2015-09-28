c702f643da0de2e1cb2ab8e98470db6ad34485fd
James Strachan <james.strachan@gmail.com>
2015-09-28 11:58:38 +0100

    added interface

---
+++ d.ts/developer/ts/project.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../includes.d.ts" />
+/// <reference path="../../kubernetes/ts/kubernetesHelpers.d.ts" />
+/// <reference path="developerHelpers.d.ts" />
+declare module Developer {
+    var ProjectController: ng.IModule;
+}
