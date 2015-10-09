b53ff0df4a820ffb0800587eb63ecd2507d27884
James Strachan <james.strachan@gmail.com>
2015-10-09 16:50:04 +0100

    remove old plugin

--- plugins/includes.ts
+++ plugins/includes.ts
@@ -1,4 +1,3 @@
-/// <reference path="../libs/hawtio-forge/defs.d.ts"/>
 /// <reference path="../libs/hawtio-forms/defs.d.ts"/>
 /// <reference path="../libs/hawtio-ui/defs.d.ts"/>
 /// <reference path="../libs/hawtio-utilities/defs.d.ts"/>
