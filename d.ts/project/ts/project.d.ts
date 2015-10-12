16389b3106632c74a60bba086895eaf2f2a4d22f
James Strachan <james.strachan@gmail.com>
2015-10-12 09:20:59 +0100

    removed old cruft

--- d.ts/project/ts/project.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../includes.d.ts" />
-/// <reference path="projectHelpers.d.ts" />
-/// <reference path="projectPlugin.d.ts" />
-declare module Project {
-    var ProjectController: ng.IModule;
-}
