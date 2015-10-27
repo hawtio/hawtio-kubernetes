22e5a2577c8572f402cd7836b72559bf71d4104d
Stan Lewis <slewis@fusesource.com>
2015-10-27 15:05:11 -0400

    More form tweaks, need to enhance hawtio-forms a bit

--- plugins/kubernetes/ts/buildConfigEdit.ts
+++ plugins/kubernetes/ts/buildConfigEdit.ts
@@ -4,21 +4,100 @@
 
 module Kubernetes {
 
-  export var BuildConfigEditController = controller("BuildConfigEditController",
-    ["$scope", "$element", "KubernetesModel", "KubernetesState", "KubernetesSchema", "$templateCache", "$location", "$routeParams", "$http", "$timeout", "KubernetesApiURL", "K8SClientFactory",
-      ($scope, $element, KubernetesModel:Kubernetes.KubernetesModelService, KubernetesState, KubernetesSchema,
-       $templateCache:ng.ITemplateCacheService, $location:ng.ILocationService, $routeParams, $http, $timeout, KubernetesApiURL, K8SClientFactory) => {
+  export var BuildConfigEditController = _module.controller("Kubernetes.BuildConfigEditController", ($scope, $element, KubernetesModel:Kubernetes.KubernetesModelService, KubernetesState, KubernetesSchema, $templateCache:ng.ITemplateCacheService, $location:ng.ILocationService, $routeParams, $http, $timeout, KubernetesApiURL, K8SClientFactory, SchemaRegistry:HawtioForms.SchemaRegistry) => {
 
         $scope.kubernetes = KubernetesState;
         $scope.model = KubernetesModel;
         $scope.id = $routeParams["id"];
         $scope.schema = KubernetesSchema;
-        $scope.config = KubernetesSchema.definitions.os_build_BuildConfig;
-        $scope.specConfig = KubernetesSchema.definitions.os_build_BuildConfigSpec;
+        var specConfig = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.BuildConfigSpec');
+        var gitBuildSource = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.GitBuildSource');
+        var buildSource = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.BuildSource');
+        var buildOutput = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.BuildOutput');
+        var resources = SchemaRegistry.getSchema('io.fabric8.kubernetes.api.model.ResourceRequirements');
+        var revision = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.SourceRevision');
+        var strategy = SchemaRegistry.getSchema('io.fabric8.openshift.api.model.BuildStrategy');
 
-        $scope.specConfig.controls = ['source', '*'];
+        // $scope.config = KubernetesSchema.definitions.os_build_BuildConfig;
+        //$scope.specConfig = KubernetesSchema.definitions.os_build_BuildConfigSpec;
+        //
+        specConfig.properties['triggers']['label-attributes'] = {
+          style: 'display: none;'
+        };
+
+        gitBuildSource.controls = ['uri', 'ref', '*'];
+        buildSource.properties['type'].type = 'hidden';
+        buildSource.properties['type']['default'] = 'Git';
+        buildSource.controls = ['git', 'contextDir', 'sourceSecret', '*'];
+
+        gitBuildSource['hideLegend'] = true;
+        buildSource['hideLegend'] = true;
+        buildOutput['hideLegend'] = true;
+        resources['hideLegend'] = true;
+        revision['hideLegend'] = true;
+        strategy['hideLegend'] = true;
+
+        strategy.controls = ['type', '*'];
+        strategy.properties['type'] = {
+          type: 'string',
+          enum: ['Custom', 'Docker', 'Source']
+        };
+        // TODO this requires some support in hawtio-forms
+        strategy.properties['customStrategy']['control-group-attributes'] = {
+          'ng-show': "entity.strategy.type == 'Custom'"
+        };
+        strategy.properties['dockerStrategy']['control-group-attributes'] = {
+          'ng-show': "entity.strategy.type == 'Docker'"
+        };
+        strategy.properties['sourceStrategy']['control-group-attributes'] = {
+          'ng-show': "entity.strategy.type == 'Source'"
+        };
+
+        // re-arranging the controls
+        //specConfig.controls = ['source', '*'];
+        
+        // tabs
+        specConfig.tabs = {
+          "Source": ["source"],
+          "Revision": ["revision"],
+          "Output": ["output"],
+          "Resources": ["resources"],
+          "Strategy": ["strategy"],
+          "Triggers": ["triggers"],
+          "Service Account": ["serviceAccount"]
+        };
+        /*
+         * wizard, needs an 'onFinish' function in the scope
+        specConfig.wizard = <any>{
+          pages: {
+            Source: {
+              controls: ["source"]
+            },
+            Revision: {
+              controls: ["revision"]
+            },
+            Output: {
+              controls: ["output"]
+            },
+            Resources: {
+              controls: ["resources"]
+            },
+            Strategy: {
+              controls: ["strategy"]
+            },
+            Triggers: {
+              controls: ["triggers"]
+            },
+            "Service Account": {
+              controls: ["serviceAccount"]
+            }
+          }
+        };
+        */
 
         Kubernetes.initShared($scope, $location, $http, $timeout, $routeParams, KubernetesModel, KubernetesState, KubernetesApiURL);
+
+        $scope.specConfig = specConfig;
 
         $scope.buildConfigClient = K8SClientFactory.create("buildconfigs", $scope.namespace);
 
@@ -126,5 +205,5 @@ module Kubernetes {
             Core.$apply($scope);
           }
         }
-      }]);
+      });
 }
