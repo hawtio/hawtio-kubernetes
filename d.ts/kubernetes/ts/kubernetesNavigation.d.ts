30cb57b11a654743b02407e9b216d1fc31bba76e
James Strachan <james.strachan@gmail.com>
2015-11-18 11:35:06 +0000

    added generated files

---
+++ d.ts/kubernetes/ts/kubernetesNavigation.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../../includes.d.ts" />
+/// <reference path="kubernetesHelpers.d.ts" />
+declare module Kubernetes {
+    function selectSubNavBar($scope: any, tabName: any, newSubTabLabel: any): void;
+}
