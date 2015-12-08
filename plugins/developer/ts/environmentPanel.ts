34ba792111a3d6daf249ca48b0c7a492933b666a
Stan Lewis <slewis@fusesource.com>
2015-12-08 13:23:28 -0500

    Bring back the old overview page without the dashboard

--- plugins/developer/ts/environmentPanel.ts
+++ plugins/developer/ts/environmentPanel.ts
@@ -5,38 +5,14 @@ module Developer {
 
     $scope.envVersions = {};
     $scope.model = KubernetesModel;
+    $scope.env = $scope.$eval('env');
+    $scope.buildConfig = $scope.$eval('entity');
 
     var caches = {};
 
     Kubernetes.initShared($scope, $location, $http, $timeout, $routeParams, KubernetesModel, KubernetesState, KubernetesApiURL);
 
-    var namespace = $routeParams['namespace'];
-    var projectId = $routeParams['projectId'];
-    var label = $routeParams['label'];
-
-    function onBuildConfigs(buildConfigs) {
-      _.forEach(buildConfigs, (buildConfig:any) => {
-        if (KubernetesAPI.getName(buildConfig) === projectId) {
-          var sortedBuilds = null;
-          Kubernetes.enrichBuildConfig(buildConfig, sortedBuilds);
-          _.forEach(buildConfig.environments, (env:any) => {
-            if (label === env.label) {
-              loadProjectVersions($scope, $element, buildConfig, env, env.namespace, $scope.envVersions, caches);
-              $scope.env = env;
-            }
-          });
-        }
-      });
-      Core.$apply($scope);
-    }
-
-    if (KubernetesModel.buildconfigs) {
-      onBuildConfigs(KubernetesModel.buildconfigs);
-    }
-
-    $scope.$watch('model.buildconfigs', onBuildConfigs);
-
+    loadProjectVersions($scope, $element, $scope.buildConfig, $scope.env, $scope.env.namespace, $scope.envVersions, caches);
 
   });
-
 }
