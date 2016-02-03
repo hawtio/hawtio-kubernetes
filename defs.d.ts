838ef761aa86ba4d1df8799dbed6b14a0d6679bc
Stan Lewis <gashcrumb@gmail.com>
2016-02-03 12:01:45 -0500

    Fix typo

--- defs.d.ts
+++ defs.d.ts
@@ -22,6 +22,7 @@
 /// <reference path="d.ts/developer/ts/projects.d.ts"/>
 /// <reference path="d.ts/developer/ts/workspace.d.ts"/>
 /// <reference path="d.ts/developer/ts/workspaces.d.ts"/>
+/// <reference path="d.ts/navigation/ts/navigationPlugin.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/apps.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/breadcrumbs.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/build.d.ts"/>
@@ -64,4 +65,3 @@
 /// <reference path="d.ts/kubernetes/ts/templates.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/watcher.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/term.d.ts"/>
-/// <reference path="d.ts/navigation/ts/navigationPlugin.d.ts"/>
