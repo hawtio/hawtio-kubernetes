2724f591fe593741b872a2e8ecbe75743645bb40
James Strachan <james.strachan@gmail.com>
2016-04-18 17:18:07 +0100

    updated definitions

--- d.ts/kubernetes/ts/kubernetesNavigation.d.ts
+++ d.ts/kubernetes/ts/kubernetesNavigation.d.ts
@@ -2,4 +2,9 @@
 /// <reference path="kubernetesHelpers.d.ts" />
 declare module Kubernetes {
     function selectSubNavBar($scope: any, tabName: any, newSubTabLabel: any): void;
+    /**
+     * Navigates to the given path. If the path starts with the HawtioCore.documentBase() then its stripped off the front
+     * so that the navigation works properly on vanilla kubernetes
+     */
+    function goToPath($location: any, path: any): any;
 }
