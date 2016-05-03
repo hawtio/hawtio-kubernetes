5942c0c1fad054812144d7e016a5ca2bbda4e71c
James Strachan <james.strachan@gmail.com>
2016-05-03 08:41:30 +0100

    support delete of environments

--- plugins/developer/ts/projectEnvironments.ts
+++ plugins/developer/ts/projectEnvironments.ts
@@ -21,36 +21,37 @@ module Developer {
         $scope.subTabConfig = Developer.createWorkspaceSubNavBars();
 
         $scope.tableConfig = {
-             data: 'model.environments',
-             showSelectionCheckbox: true,
-             enableRowClickSelection: true,
-             multiSelect: true,
-             selectedItems: [],
-             filterOptions: {
-               filterText: $location.search()["q"] || ''
-             },
-             columnDefs: [
-               {
-                 field: 'name',
-                 displayName: 'Environment',
-                 cellTemplate: $templateCache.get("environmentNameTemplate.html")
-               },
-               {
-                 field: 'name',
-                 displayName: 'Actions',
-                 cellTemplate: $templateCache.get("environmentEditTemplate.html")
-               },
-               {
-                 field: 'namespace',
-                 displayName: 'Namespace'
-               },
-               {
-                 field: 'clusterUrl',
-                 displayName: 'Cluster URL'
-               }
-             ]
-           };
+          data: 'model.environments',
+          showSelectionCheckbox: true,
+          enableRowClickSelection: true,
+          multiSelect: true,
+          selectedItems: [],
+          filterOptions: {
+            filterText: $location.search()["q"] || ''
+          },
+          columnDefs: [
+            {
+              field: 'name',
+              displayName: 'Environment',
+              cellTemplate: $templateCache.get("environmentNameTemplate.html")
+            },
+            {
+              field: 'name',
+              displayName: 'Actions',
+              cellTemplate: $templateCache.get("environmentEditTemplate.html")
+            },
+            {
+              field: 'namespace',
+              displayName: 'Namespace'
+            },
+            {
+              field: 'clusterUrl',
+              displayName: 'Cluster URL'
+            }
+          ]
+        };
 
+        var kubeClient = Kubernetes.createKubernetesClient("configmaps");
 
         $scope.editEntityLink = (entity) => {
           if (entity) {
@@ -75,6 +76,45 @@ module Developer {
           }
         };
 
+        $scope.deletePrompt = (selected) => {
+          UI.multiItemConfirmActionDialog(<UI.MultiItemConfirmActionOptions>{
+            collection: selected,
+            index: 'label',
+            onClose: (result:boolean) => {
+              if (result) {
+                function deleteSelected(selected, next) {
+                  if (next) {
+                    deleteEntity(next, () => {
+                      deleteSelected(selected, selected.shift());
+                    });
+                  }
+                }
+                deleteSelected(selected, selected.shift());
+              }
+            },
+            title: 'Delete Environments',
+            action: 'The following Environments will be deleted:',
+            okText: 'Delete',
+            okClass: 'btn-danger',
+            custom: "This operation is permanent once completed!",
+            customClass: "alert alert-warning"
+          }).open();
+        };
 
+        function deleteEntity(entity, callback) {
+          var key = (entity || {}).key;
+          if (key) {
+            var configMap = Kubernetes.getNamed($scope.model.configmaps, Kubernetes.environemntsConfigMapName);
+            if (configMap) {
+              delete configMap.data[key];
+              kubeClient.put(configMap, (data) => {
+                log.info("Deleted environment " + key);
+                if (angular.isFunction(callback)) {
+                  callback();
+                }
+              });
+            }
+          }
+        }
       }]);
 }
