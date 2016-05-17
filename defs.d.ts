a307f5c019df5630074fff28ab0dd79b7bbceadb
Stan Lewis <gashcrumb@gmail.com>
2016-05-17 16:02:32 -0400

    Rebuild

--- defs.d.ts
+++ defs.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="d.ts/includes.d.ts"/>
-/// <reference path="d.ts/navigation/ts/navigationPlugin.d.ts"/>
 /// <reference path="d.ts/developer/ts/developerHelpers.d.ts"/>
 /// <reference path="d.ts/developer/ts/developerPlugin.d.ts"/>
 /// <reference path="d.ts/kubernetes/ts/kubernetesInterfaces.d.ts"/>
